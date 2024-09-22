3dac4177b69124537cb2cfabead77c1ecab3d712
mkpage1337 <mrku1337@gmail.com>
2024-09-22 14:47:45 +0200

    refactor

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import CrudApp from './components/CrudApp';
+import React from "react";
+import CrudApp from "./components/CrudApp";
 
 const App = () => {
-	return (
-		<div className='App'>
-			<CrudApp />
-		</div>
-	);
+  return (
+    <div className="App">
+      <CrudApp />
+    </div>
+  );
 };
 
 export default App;
